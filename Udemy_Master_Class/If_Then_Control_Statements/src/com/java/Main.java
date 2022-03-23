862ef8c9b1edab002a9e6ccfcece3cb39f56d2c2
Daniel Ramirez <zoerunner@gmail.com>
2022-03-22 18:55:42 -0700

    Methods

--- Udemy_Master_Class/If_Then_Control_Statements/src/com/java/Main.java
+++ Udemy_Master_Class/If_Then_Control_Statements/src/com/java/Main.java
@@ -3,31 +3,36 @@ package com.java;
 public class Main {
 
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
 
-//        if (score < 5000 && score > 1000) {
-//            System.out.println("Your score was less than 5000");
-//        } else if (score < 1000) {
-//            System.out.println("Your score was less than 1000");
-//        } else {
-//            System.out.println("Got here");
-//        }
+        int highScore = calculateScore( true, 800, 5, 100);
+        System.out.println("Your final score was " + highScore);
+
+        highScore = calculateScore( true, 10000, 8, 200);
+        System.out.println("Your final score was " + highScore);
+
+        displayHighScorePosition("P1", calculateHighScorePosition(1500));
+        displayHighScorePosition("P2", calculateHighScorePosition(900));
+        displayHighScorePosition("P3", calculateHighScorePosition(400));
+        displayHighScorePosition("P4", calculateHighScorePosition(50));
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         if (gameOver){
             int finalScore = score + ( levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
+            finalScore += 2000;
+            return finalScore;
         }
+        return -1;
+    }
 
-        boolean gameOverTwo = true;
-        int scoreTwo = 10000;
-        int levelCompletedTwo = 8;
-        int bonusTwo = 200;
+    public static void displayHighScorePosition(String name, int position) {
+        System.out.println(name + " managed to get into position " + position + " on the high score table");
+    }
 
-        if (gameOverTwo){
-            int finalScore = scoreTwo + (levelCompletedTwo * bonusTwo);
-            System.out.println("New finalScore is " + finalScore);
-        }
+    public static int calculateHighScorePosition(int score){
+        if (score >= 1000) return 1;
+        if (score >= 500) return 2;
+        if (score >= 100) return 3;
+        return 4;
     }
 }
