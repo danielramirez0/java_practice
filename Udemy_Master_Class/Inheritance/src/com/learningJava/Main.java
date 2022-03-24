6a2c7f3623ff6feeaafe1d8e8330e29bc1e05105
Daniel Ramirez <zoerunner@gmail.com>
2022-03-24 12:53:08 -0700

    Inheritance

---
+++ Udemy_Master_Class/Inheritance/src/com/learningJava/Main.java
@@ -0,0 +1,14 @@
+package com.learningJava;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Animal animal = new Animal("Animal", 1,1,5,5);
+
+        Dog dog = new Dog("Yorkie",2, 4, 1, 20,1,1, "long silky");
+        dog.eat();
+        dog.walk();
+        dog.run();
+
+    }
+}
