ac64f578ee83fe0f3c64ab896797e110caf7b758
Daniel Ramirez <zoerunner@gmail.com>
2022-04-06 18:32:40 -0700

    Refactor to use binary search methods from Collections methods

--- Udemy_Master_Class/CollectionsOverview/src/com/learningjava/Main.java
+++ Udemy_Master_Class/CollectionsOverview/src/com/learningjava/Main.java
@@ -6,6 +6,18 @@ public class Main {
         Theatre cinemark = new Theatre("Cinemark", 8, 12);
         cinemark.getSeats();
         if (cinemark.reserveSeat("H11")){
+            // Without binary search method from collections reserving involves searching every single
+            // item in the array until the proper item is found to update (inefficient)
+            System.out.println("Pay for seat");
+        }else {
+            System.out.println("Sorry, seat is taken");
+        }
+        if (cinemark.reserveSeat("D12")){
+            System.out.println("Pay for seat");
+        }else {
+            System.out.println("Sorry, seat is taken");
+        }
+        if (cinemark.reserveSeat("K12")){
             System.out.println("Pay for seat");
         }else {
             System.out.println("Sorry, seat is taken");
--- Udemy_Master_Class/CollectionsOverview/src/com/learningjava/Theatre.java
+++ Udemy_Master_Class/CollectionsOverview/src/com/learningjava/Theatre.java
@@ -16,9 +16,9 @@ public class Theatre {
     public Theatre(String name, int rows, int seatsPerRow) {
         this.name = name;
         int lastRow = 'A' + (rows - 1); // Uses ASCII values for calculations
-        for (char row = 'A'; row <= lastRow; row++){
+        for (char row = 'A'; row <= lastRow; row++) {
             for (int seatNumber = 1; seatNumber <= seatsPerRow; seatNumber++) {
-                Seat seat = new Seat (row + String.format("%02d", seatNumber));
+                Seat seat = new Seat(row + String.format("%02d", seatNumber));
                 seats.add(seat);
             }
         }
@@ -28,31 +28,42 @@ public class Theatre {
         return name;
     }
 
-    public boolean reserveSeat(String seatNumber){
-        Seat requestedSeat = null;
-        for (Seat seat: seats){
-            if (seat.getSeatNumber().equals(seatNumber)){
-                requestedSeat = seat;
-                break;
-            }
-        }
-        
-        if (requestedSeat == null){
-            System.out.println("There is no seat " + seatNumber);
+    public boolean reserveSeat(String seatNumber) {
+//        Seat requestedSeat = null;
+        // Because of the way Binary Searching works, this will take substantially less effort to find a match
+        // e.g.
+        // 1024 elements is 2^10, so no more than 10 checks in an array of 1024 items
+        // 1,048,576 elements is 2^20, so no more than 20 checks in an array of over a million items
+        Seat requestedSeat = new Seat(seatNumber);
+        int foundSeat = Collections.binarySearch(seats, requestedSeat, null);
+        if (foundSeat >= 0) {
+            return seats.get(foundSeat).toggleReservation();
+        }else {
             return false;
         }
-        return requestedSeat.toggleReservation();
+//        for (Seat seat : seats) {
+//            if (seat.getSeatNumber().equals(seatNumber)) {
+//                requestedSeat = seat;
+//                break;
+//            }
+//        }
+//
+//        if (requestedSeat == null) {
+//            System.out.println("There is no seat " + seatNumber);
+//            return false;
+//        }
+//        return requestedSeat.toggleReservation();
     }
-    
+
     // for testing
-    public void  getSeats(){
+    public void getSeats() {
         for (Seat seat :
                 seats) {
             System.out.println(seat.getSeatNumber());
         }
     }
 
-    private class Seat{
+    private class Seat implements Comparable<Seat> {
         private final String seatNumber;
         private boolean reserved = false;
 
@@ -60,7 +71,7 @@ public class Theatre {
             this.seatNumber = seatNumber;
         }
 
-        public boolean toggleReservation(){
+        public boolean toggleReservation() {
             this.reserved = !this.reserved;
             System.out.println("Seat " + seatNumber + (this.reserved ? " reserved" : " canceled"));
             return this.reserved;
@@ -68,6 +79,14 @@ public class Theatre {
 
         public String getSeatNumber() {
             return seatNumber;
+        }
+
+        @Override
+        public int compareTo(Seat seat) {
+            // This method fulfills the interface requirements
+            // gives ability to use a different type of set for searching
+            // much more efficient
+            return this.seatNumber.compareToIgnoreCase(seat.getSeatNumber()); // simply using the compareTo method built into the String class
         }
     }
 
