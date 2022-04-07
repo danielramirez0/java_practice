008a2ea71c58a35c4c1fb0ee314ae4a77ef34cd0
Daniel Ramirez <zoerunner@gmail.com>
2022-04-07 13:05:11 -0700

    Makes Location class immutable using standard techniques

--- Udemy_Master_Class/Collections_Maps/src/com/learningjava/Location.java
+++ Udemy_Master_Class/Collections_Maps/src/com/learningjava/Location.java
@@ -3,21 +3,26 @@ package com.learningjava;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Location {
+public final class Location {
     private final int locationId;
     private final String description;
     private final Map<String, Integer> exits;
 
-    public Location(int locationId, String description) {
+    public Location(int locationId, String description, Map<String, Integer> exits) {
         this.locationId = locationId;
         this.description = description;
-        this.exits = new HashMap<>();
+//        this.exits = new HashMap<>();
+        if (exits != null) {
+            this.exits = new HashMap<>(exits);
+        }else {
+            this.exits = new HashMap<>();
+        }
         this.exits.put("Q", 0);
     }
 
-    public void addExit(String direction, int location) {
-        exits.put(direction, location);
-    }
+//    public void addExit(String direction, int location) {
+//        exits.put(direction, location);
+//    }
 
     public int getLocationId() {
         return locationId;
